620ffff904bd4bdd60857bd4a8b9960c25af59db
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-14 15:29:51 -0400

    Updated version

--- DuelYang/src/duel/CharacterA.java
+++ DuelYang/src/duel/CharacterA.java
@@ -2,4 +2,20 @@ package duel;
 
 public class CharacterA {
 
+	public void taunt()
+	{
+		printLn("Show me your moves!");
+	}
+	public String getName()
+	{
+		return "Jason Yang";
+	}
+	public void setStartingHp(int hp)
+	{
+
+	}
+	public int getHP()
+	{
+
+	}
 }
