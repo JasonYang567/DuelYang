433700388e939099b2adaf754439788111dc0855
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-15 15:33:54 -0400

    edited version of Character B

--- DuelYang/src/duel/CharacterB.java
+++ DuelYang/src/duel/CharacterB.java
@@ -3,7 +3,13 @@ package duel;
 public class CharacterB implements Dueler {
 	
 	int health;
-
+	double choicePercentage;
+	int choice; 
+	
+	public CharacterB()
+	{
+		
+	}
 	public void taunt()
 	{
 		System.out.println("What are you even doing?");
@@ -14,10 +20,40 @@ public class CharacterB implements Dueler {
 	}
 	public void setStartingHP(int hp)
 	{
-		health = hp;
+		this.health = hp;
 	}
 	public int getHP()
 	{
-		return health; 
+		return this.health; 
+	}
+	public boolean determineIfOpponentIsFair()
+	{
+		return true;
+	}
+	public int getAction( Object caller )
+	{
+		if(caller == "duel")
+		{
+			choicePercentage = Math.random(); 
+			if(choicePercentage<=.33)
+			{
+				return 1; 
+			}
+			else 
+			{
+				if (choicePercentage>1/3 && choicePercentage<=2/3)
+				{
+					return 2; 
+				}
+				else 
+				{
+					return 3;
+				}
+			}
+		}
+	}
+	public void hit(characterB x )
+	{
+		this.health -= 10; 
 	}
 }
